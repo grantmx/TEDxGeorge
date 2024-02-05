efe42101297a2bbe4da7b44001d0453de927345e
grantmx <grantmx@users.noreply.github.com>
2024-02-05 06:46:31 -0500

    added mobile support

--- app/page.js
+++ app/page.js
@@ -3,14 +3,28 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between">	
-			<Image
-				src="/tedxgeorge-background.jpg"
-				alt="TEDxGeorge"
-				fill
-				placeholder="blur"
-				blurDataURL="/background-placeholder.jpg"
-				style={{ objectFit: "cover" }}
-			/>	
+			<picture>
+				<source
+					media="(max-width: 768px)"
+					srcSet="/tedxgeorge-background-mobile.jpg"
+					type="image/jpg"
+				/>
+				<source
+					media="(min-width: 767px)"
+					srcSet="/tedxgeorge-background.jpg"
+					type="image/jpg"
+				/>
+
+				<Image
+					src="/tedxgeorge-background.jpg"
+					alt="TEDxGeorge"
+					fill
+					placeholder="blur"
+					blurDataURL="/background-placeholder.jpg"
+					style={{ objectFit: "cover" }}
+				/>	
+			</picture>
+			
 		</main>
 	);
 }
