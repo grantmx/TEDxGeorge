dc0b1bae5440723359350d7fba5c21f8608ddbee
grantmx <grantmx@users.noreply.github.com>
2024-02-05 06:13:52 -0500

    placeholder

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between">	
+			<Image
+				src="/tedxgeorge-background.jpg"
+				alt="TEDxGeorge"
+				fill
+				style={{ objectFit: "cover" }}
+			/>	
+		</main>
+	);
+}
