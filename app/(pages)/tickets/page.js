496ee15b67665771693df1e54ea8c152c601dd76
grantmx <grantmx@users.noreply.github.com>
2024-04-25 15:44:20 -0400

    pricing changes

--- app/(pages)/tickets/page.js
+++ app/(pages)/tickets/page.js
@@ -1,6 +1,9 @@
 import Utils from "@/styles/globals/utils.module.scss"
 import Style from "../Page.module.scss"
 import clsx from "clsx"
+import { features, ticketList } from "@/lib/ticketList"
+import Checkmark from "@/components/icons/Checkmark"
+import Cross from "@/components/icons/Cross"
 
 
 export const metadata = {
@@ -22,34 +25,76 @@ function Tickets(){
 
                     <article>
                         <p className={clsx(Utils.fs_1)}>
-                            We in the TEDx Community view our sponsors as partners who are joining us on a journey. This is a project that we're investing in together. 
-                        </p>
-                        <p>
-                            <a href="/TEDxGeorge_Sponsors_Welcome_Guide.pdf" download={true} className="btn btn-danger fs-1 px-5">
-                                Download Sponsorship Guide
-                            </a>
+                            "The Brave Ones" Event Registration
                         </p>
                     </article>
                 </div>
             </section>
 
+            <section className="container">
+                <div className="row align-items-start">
+                    <div className="col-3">
+                        <h2 className={clsx(Utils.fs_2)}>
+                            Ticket Comparison
+                        </h2>
+                    </div>
 
-            <section className={clsx("container", Utils.mb_8)}>
-                <hr className={Utils.my_6} />
+                    {ticketList.map((ticket, index) => {
+                        return(
+                            <div className="col text-center" key={ticket.type}>
+                                <h2 className={clsx(Utils.fs_4, "mb-0")}>
+                                    {ticket.type}
+                                </h2>
+                                <small>
+                                    {ticket.description}
+                                </small>
+                            </div>
+                        )
+                    })}
+                </div>
 
-                <article className="text-center d-flex flex-column align-items-center">
-                    <h3 className={Utils.fs_1}>
-                        In-Kind Donations
-                    </h3>
 
-                    <p className="w-75 fs-1">
-                        For information on how to support TEDxGeorge through in-kind donations of products or services, email <a href="mailto:info@tedxgeorge.com">info@tedxgeorge.com</a>
-                    </p>
-                </article>
-            </section>
+                <div className="row">
+                    <div className="col-3 p-0">
+                        {Object.keys(features).map((feature, i) => {
+                            return (
+                                <div className={clsx("col p-4", (i % 2) && Style.zebraStripe, Style.cell)} key={feature}>
+                                    {features[feature].title}
+                                    {features[feature]?.toolTip}
+                                </div>
+                            )
+                        })}
 
+                        <div className={clsx(Utils.fs_4, "col p-4")}>
+                            Ticket Price
+                        </div>
+                    </div>
 
-           
+                    {ticketList.map((ticket) => {
+                        return(
+                            <div className="col p-0" key={ticket.price}>
+                                {ticket.features.map((feature, i) => {
+                                    return (
+                                        <div className={clsx("py-4 d-flex justify-content-center", (i % 2) && Style.zebraStripe, Style.cell)} key={feature.title}>
+                                            {feature.included ? <Checkmark /> : <Cross />}
+                                        </div>
+                                    )
+                                })}
+
+                                <div className="py-4 d-flex align-items-center flex-column">
+                                    <span className={clsx(Utils.fs_4)}>
+                                        {ticket.price}
+                                    </span>
+
+                                    <a href="#" className="btn btn-success py-2 px-4 fs-2">
+                                        Register Now
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section> 
         </>
     )
 }
---
+++ components/icons/Checkmark.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Checkmark(){
+    return(
+        <svg id="icon-checkmark" viewBox="0 0 32 32" width={24} height={24} fill="#27ae60">
+            <path d="M27 4l-15 15-7-7-5 5 12 12 20-20z"></path>
+        </svg>
+    )
+}
