1292089135e0051f3a3b87c8670ca9b966a8257d
grantmx <grantmx@users.noreply.github.com>
2024-08-25 14:03:18 -0400

    Update page.js

--- app/(pages)/speakers/page.js
+++ app/(pages)/speakers/page.js
@@ -38,7 +38,10 @@ function Speakers(){
             </section>
 
 
-            <section className="container mb-5">
+            <section className="container">
+                <hr className={Utils.my_6} />
+                
+                
                 <div className="row mt-4">
                     {speakers.map((speaker) => {
                         return(
@@ -58,25 +61,32 @@ function Speakers(){
 
 
             <section className="container mb-5">
-                <h2 className={Utils.fs_2}>
-                    Curation Approach at TEDxGeorge
-                </h2>
 
-                <p>At TEDxGeorge, we welcome individuals from all backgrounds, disciplines, and cultures who seek to explore new ideas and connect with others. We understand that not every curation decision will resonate with everyone, but our selections are rooted in principles that reflect our commitment to inclusivity and thoughtful debate.</p>
+                <div className="row">
+                    <heading className="col-lg-4 col-12">
+                        <h2 className={Utils.fs_2}>
+                            Curation Approach at TEDxGeorge
+                        </h2>
+                    </heading>
 
-                <h3 className={Utils.fs_3}>
-                    Inclusion
-                </h3>
+                    <article className="col-lg-8 col-12">
+                        <p>At TEDxGeorge, we welcome individuals from all backgrounds, disciplines, and cultures who seek to explore new ideas and connect with others. We understand that not every curation decision will resonate with everyone, but our selections are rooted in principles that reflect our commitment to inclusivity and thoughtful debate.</p>
 
-                <p>Ideas know no boundaries—they are not confined by geography, religion, politics, or culture. TEDxGeorge believes that ideas belong to everyone, regardless of race, gender, identity, or social class. With a global perspective, we seek to source diverse ideas and ensure they are accessible to all. We know that powerful ideas, when shared with care, have the potential to unite different groups and build a collective vision for a better future.</p>
+                        <h3 className={Utils.fs_3}>
+                            Inclusion
+                        </h3>
 
-                <h3 className={Utils.fs_3}>
-                    Debate
-                </h3>
+                        <p>Ideas know no boundaries—they are not confined by geography, religion, politics, or culture. TEDxGeorge believes that ideas belong to everyone, regardless of race, gender, identity, or social class. With a global perspective, we seek to source diverse ideas and ensure they are accessible to all. We know that powerful ideas, when shared with care, have the potential to unite different groups and build a collective vision for a better future.</p>
 
-                <p>Our talks are the beginning, not the conclusion, of discussions. TEDxGeorge values skepticism and the role it plays in refining ideas, revealing their strengths and areas for improvement. We encourage conversations after our talks as a way to gain clarity and explore how these ideas can be applied. Our commitment is to provide an open and respectful environment where vigorous debate can thrive. We reject bigotry and hate speech, instead promoting constructive conversations as a means to bridge gaps and build understanding.</p>
-                <p>By adhering to these guiding principles, TEDxGeorge remains true to TED's ethos, creating a platform where ideas can inspire, challenge, and drive positive change within our community.</p>
+                        <h3 className={Utils.fs_3}>
+                            Debate
+                        </h3>
+
+                        <p>Our talks are the beginning, not the conclusion, of discussions. TEDxGeorge values skepticism and the role it plays in refining ideas, revealing their strengths and areas for improvement. We encourage conversations after our talks as a way to gain clarity and explore how these ideas can be applied. Our commitment is to provide an open and respectful environment where vigorous debate can thrive. We reject bigotry and hate speech, instead promoting constructive conversations as a means to bridge gaps and build understanding.</p>
+                        <p>By adhering to these guiding principles, TEDxGeorge remains true to TED's ethos, creating a platform where ideas can inspire, challenge, and drive positive change within our community.</p>
                             
+                    </article>
+                </div>                     
             </section>
         </>
     )
