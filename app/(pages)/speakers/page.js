b38a7f83095884b158bb99560c5a783051ee7485
grantmx <grantmx@users.noreply.github.com>
2024-08-25 13:58:34 -0400

    Update page.js

--- app/(pages)/speakers/page.js
+++ app/(pages)/speakers/page.js
@@ -29,6 +29,10 @@ function Speakers(){
                         <p className={Utils.fs_1}>
                             Our speaker's ideas will inspire you to seek your highest potential.
                         </p>
+
+                        <p>
+                            TEDxGeorge is guided by the broader TED mission to uncover and share ideas that inspire, challenge, and create impact. As we navigate a world with growing divides, our goal is to bring together a wide range of voices, encouraging dialogue and deeper understanding among our community. We believe that valuable lessons can be learned from every speaker, even when their perspectives differ from our own. Our platform is dedicated to fostering meaningful conversations between people with diverse viewpoints.
+                        </p>
                     </article>
                 </div>
             </section>
@@ -38,7 +42,7 @@ function Speakers(){
                 <div className="row mt-4">
                     {speakers.map((speaker) => {
                         return(
-                            <div className="col-6 col-lg-3 mb-4" key={speaker.slug}>
+                            <figure className="col-6 col-lg-3 mb-4" key={speaker.slug}>
                                 <Image 
                                     src={`https://dummyimage.com/400x400/111/fff&text=Announcing+Soon`}
                                     alt="Event 1" 
@@ -46,10 +50,33 @@ function Speakers(){
                                     height={400} 
                                     className={clsx(Utils.w_100, Utils.h_auto, "object-fit-contain")}
                                 />
-                            </div>
+                            </figure>
                         )
                     })}
                 </div>
+            </section>
+
+
+            <section className="container mb-5">
+                <h2 className={Utils.fs_2}>
+                    Curation Approach at TEDxGeorge
+                </h2>
+
+                <p>At TEDxGeorge, we welcome individuals from all backgrounds, disciplines, and cultures who seek to explore new ideas and connect with others. We understand that not every curation decision will resonate with everyone, but our selections are rooted in principles that reflect our commitment to inclusivity and thoughtful debate.</p>
+
+                <h3 className={Utils.fs_3}>
+                    Inclusion
+                </h3>
+
+                <p>Ideas know no boundaries—they are not confined by geography, religion, politics, or culture. TEDxGeorge believes that ideas belong to everyone, regardless of race, gender, identity, or social class. With a global perspective, we seek to source diverse ideas and ensure they are accessible to all. We know that powerful ideas, when shared with care, have the potential to unite different groups and build a collective vision for a better future.</p>
+
+                <h3 className={Utils.fs_3}>
+                    Debate
+                </h3>
+
+                <p>Our talks are the beginning, not the conclusion, of discussions. TEDxGeorge values skepticism and the role it plays in refining ideas, revealing their strengths and areas for improvement. We encourage conversations after our talks as a way to gain clarity and explore how these ideas can be applied. Our commitment is to provide an open and respectful environment where vigorous debate can thrive. We reject bigotry and hate speech, instead promoting constructive conversations as a means to bridge gaps and build understanding.</p>
+                <p>By adhering to these guiding principles, TEDxGeorge remains true to TED's ethos, creating a platform where ideas can inspire, challenge, and drive positive change within our community.</p>
+                            
             </section>
         </>
     )
