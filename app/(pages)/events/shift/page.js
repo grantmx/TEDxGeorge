2d2e411466bf14dad1117013299dc9d79dea76aa
grantmx <grantmx@users.noreply.github.com>
2025-07-28 07:11:29 -0400

    Update page.js

--- app/(pages)/events/shift/page.js
+++ app/(pages)/events/shift/page.js
@@ -4,7 +4,9 @@ import Image from "next/image";
 import hero from "@/public/quicket-shift-tedx.png"
 import mobileHero from "@/public/shift-concept-design.png"
 import clsx from "clsx";
-
+import { speakerList } from "@/lib/speakerList"
+import sortBy from "@/lib/utils/sortBy"
+import SpeakerCard from "@/components/speakers/SpeakerCard"
 
 export const metadata = {
     title: "Shift",
@@ -18,7 +20,10 @@ export const metadata = {
 
 
 
-function Shift(){
+async function Shift(){
+    const speakers = sortBy(Object.values(speakerList), "last_name")
+        .filter((speaker) => speaker.active.some((activeYear) => activeYear === 2025));
+
 
     return(
         <>
@@ -36,29 +41,36 @@ function Shift(){
                          
 					/>
 				</picture>
-
-               
 			</section>
 
             
 
             <section className="container text-center d-flex justify-content-center mt-5 pt-5">
                 <div className={Style.heading}>
-                    <h2 className={clsx(Utils.fs_5, "fw-light")}>
-                        About Shift 2025
-                    </h2>
+                    <h1 className={clsx(Utils.fs_5, "fw-light")}>
+                        Shift 2025 &nbsp;&nbsp;|&nbsp;&nbsp; October 11, 2025 &nbsp;&nbsp;|&nbsp;&nbsp; George Arts Theater
+                    </h1>
 
                     <article>
-                        <p className={clsx(Utils.fs_3, Utils.mb_1)}>
-                            October 11, 2025 &nbsp;&nbsp;|&nbsp;&nbsp; George Arts Theater
+                        <p className={clsx(Utils.fs_3, Utils.mb_2)}>
+                            In a world of AI hallucinations, political instability, climate extremes, and digital fatigue, there's a feeling that something isn't right. That instinct is the glitch. And the glitch, if followed, becomes the SHIFT.
                         </p>
 
-                        <p className={clsx(Utils.fs_5, Utils.mb_2)}>
-                            An inspirational one day event propelling you to be challenged at every level.
-                        </p>
+                        <p>This year, the TEDxGeorge stage becomes a signal amplifier—a place where unseen disruptors are decoded and glitches become movements. We invite speakers, performers, and storytellers who are not only noticing the cracks in the system but using them as pathways for new thinking.</p>
                     </article>
                 </div>
             </section>
+
+            <section className="container mb-5">                
+                <div className="row mt-4">
+                    {speakers.map((speaker) => {
+                        return <SpeakerCard {...speaker} key={speaker?.slug} />
+                    })}
+                </div>
+            </section>
+
+
+
             
         </>
     )
